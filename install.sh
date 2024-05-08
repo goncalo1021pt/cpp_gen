977db4d2e45b6cf95f1e43c45afb5b05bb7db77e
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-05-08 12:51:12 +0100

    backup

--- install.sh
+++ install.sh
@@ -4,9 +4,18 @@
 cd ~
 
 # Create a hidden directory named cpp_gen
+if [ -d cpp_gen ]; then
+	echo "Directory cpp_gen already exists do you want to overwrite it? (y/n)"
+	read answer
+	if [ "$answer" == "y" ]; then
+		rm -rf cpp_gen
+	else
+		exit 1
+fi
+
 git clone https://github.com/goncalo1021pt/cpp_gen.git cpp_gen
 
-mv cpp_gen/cpp_gen.sh .local/bin
+mv cpp_gen/cpp_gen.sh ~/local/bin
 
 rm -f cpp_gen/install.sh
 rm -f README.md
